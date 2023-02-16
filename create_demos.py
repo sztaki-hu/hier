bd1f9434343d76445db34d34140819fea8582d7e
Daniel Horvath <hdaniel@sztaki.hu>
2023-02-16 20:34:34 +0100

    fix create_demo

--- create_demos.py
+++ create_demos.py
@@ -19,16 +19,17 @@ import torch
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--configfile", default="/cfg/config.yaml" ,help="Path of the config file")
+    parser.add_argument("--configfile", default="cfg/config.yaml" ,help="Path of the config file")
+    #parser.add_argument("--configfile", default="logs/0216_A_test_stack_blocks_sac" ,help="Path of the config file")
     parser.add_argument("--trainid", type=int, default=0 ,help="Train ID")
+    parser.add_argument("--restart", type=bool, default=False ,help="Set true if you want to restart a training")
+    parser.add_argument("--restart_epoch", type=int, default=5 ,help="The epoch number from where you want to restart the training.")
     # Example: python3 main.py --configfile /cfg/alma.yaml
     args = parser.parse_args()
 
     # Init logger ###############################################x
     current_dir = dirname(abspath(__file__))
-    config_path = current_dir + args.configfile
-    #config_path = current_dir + "/cfg/config_test.yaml"
-    logger = Logger(current_dir = current_dir, config_path = config_path, trainid = args.trainid, light_mode = True)
+    logger = Logger(current_dir = current_dir, main_args = args, light_mode = True)
     config = logger.get_config()
 
     #print(config['environment']['task']['params'])
@@ -47,18 +48,15 @@ def main():
     np.random.seed(config['general']['seed'])
 
     # Init Demo Buffer ################################################
-    if config['demo']['demo_use']:
-        demo = Demo(logger,config)
-        if demo.demo_create:
-            demo.clean_up_old_demo()
-            demo.create_demos()
-            # demo_p = Process(target=demo.create_demos, args=[])
-            # demo_p.start()
-            # demo_p.join()  
-        else:
-            demoBuffer = demo.load_demos()
-            batch = demoBuffer.get_first(10)
-            print(batch)
+    
+    demo = Demo(logger,config)
+
+    demo.clean_up_old_demo()
+    demo.create_demos()
+
+    # demoBuffer = demo.load_demos()
+    # batch = demoBuffer.get_first(10)
+    # print(batch)
                      
 
 if __name__ == '__main__':
