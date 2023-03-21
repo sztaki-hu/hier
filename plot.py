b575daaf5955896a6717d8aa0f8c3562adfa9871
Daniel Horvath <hdaniel@sztaki.hu>
2023-03-21 13:10:53 +0100

    plot more experiments

--- plot.py
+++ plot.py
@@ -21,52 +21,62 @@ def create_folder(path):
 parser = argparse.ArgumentParser()
 parser.add_argument("--logdir", default="csv_to_plot" ,help="Path of the data folder")
 parser.add_argument("--outdir", default="plots" ,help="Path of the output folder")
-parser.add_argument("--expname", default="0320_F_stack_blocks_sac",help="Path of the config file")
-parser.add_argument("--trainnum", type=int, default=3 ,help="Train ID")
+parser.add_argument("--taskname", default="stack_blocks_sac" ,help="Task name")
 args = parser.parse_args()
 
 create_folder(os.path.join(current_dir, args.outdir))
 
+expname_list = ["0320_A","0320_F"]
+expcolor_list = ["blue","orange"]
+seeds_list = [2,3]
+
 graph_name = "test_glob_checkpoint_test_return"
-graph_csv_name = "run-" + str(args.expname) +  "_" + str(0) + "_runs-tag-" + graph_name + ".csv"
-path = os.path.join(current_dir, args.logdir,graph_csv_name)
-test_ret = pd.read_csv(path)
-test_ret['Step'] /= 1000
-test_ret = test_ret.rename(columns={"Value": "Value_0"})
-test_ret = test_ret.drop(columns=['Wall time'])
-
-for i in range(1,args.trainnum):
-    graph_csv_name = "run-" + str(args.expname) +  "_" + str(i) + "_runs-tag-" + graph_name + ".csv"
+test_ret_list = []
+for expname,seed in zip(expname_list,seeds_list):
+    print(expname)
+    print(seed)
+    graph_csv_name = "run-" + expname + "_" + args.taskname +  "_" + str(0) + "_runs-tag-" + graph_name + ".csv"
     path = os.path.join(current_dir, args.logdir,graph_csv_name)
-    pivot = pd.read_csv(path)
-    
-    # Using 'Address' as the column name
-    # and equating it to the list
-    test_ret['Value_'+str(i)] = pivot['Value']
+    test_ret = pd.read_csv(path)
+    test_ret['Step'] /= 1000
+    test_ret = test_ret.rename(columns={"Value": "Value_0"})
+    test_ret = test_ret.drop(columns=['Wall time'])
 
-test_ret["id"] = test_ret.index
-print(test_ret.head())
+    for i in range(1,seed):
+        graph_csv_name = "run-" + expname + "_" + args.taskname +  "_" + str(i) + "_runs-tag-" + graph_name + ".csv"
+        path = os.path.join(current_dir, args.logdir,graph_csv_name)
+        pivot = pd.read_csv(path)
+        test_ret['Value_'+str(i)] = pivot['Value']
+
+    test_ret["id"] = test_ret.index
+    print(test_ret.head())
+    test_ret_list.append(test_ret)
 
 # Separate plotting ########################## 
 
-for i in range(args.trainnum):
-    sns.lineplot(data=test_ret, x="Step", y="Value_"+str(i))
+for i in range(len(test_ret_list)):
+    for j in range(seeds_list[i]):
+        #sns.lineplot(data=test_ret_list[i], x="Step", y="Value_"+str(j),palette=expcolor_list[i])
+        plt.plot(test_ret_list[i]['Step'],test_ret_list[i]['Value_'+str(j)],color=expcolor_list[i])
 plt.xlabel("Step (x1000)")
 plt.ylabel("Average test return")
-plt.title("Test returns of different seeds")
-figname = args.expname + '_test_res_separate.png'
+plt.title("Test returns")
+figname = 'test_res_separate.png'
 plt.savefig(os.path.join(current_dir, args.outdir, figname))
 plt.show()
 plt.clf()
 
 # SD plot ###################################
-test_ret_wide = pd.wide_to_long(test_ret, stubnames='Value', i='id', j="Step", sep='_')
+test_ret_wide = []
+for i in range(len(test_ret_list)):
+    test_ret_wide.append(pd.wide_to_long(test_ret_list[i], stubnames='Value', i='id', j="Step", sep='_'))
 
-sns.lineplot(data=test_ret_wide, x="Step", y="Value", errorbar=('ci', 95))
+for i in range(len(test_ret_wide)):
+    sns.lineplot(data=test_ret_wide[i], x="Step", y="Value", errorbar=('ci', 95))
 plt.xlabel("Step (x1000)")
 plt.ylabel("Average test return")
 plt.title("Test returns with sd 95")
-figname = args.expname + '_test_res_std.png'
+figname = 'test_res_std.png'
 plt.savefig(os.path.join(current_dir, args.outdir, figname))
 plt.show()
 plt.clf()
