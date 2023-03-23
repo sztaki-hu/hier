6833001fbecd237c739d345471b5eaa399bc985b
Daniel Horvath <hdaniel@sztaki.hu>
2023-03-23 16:21:08 +0100

    update main list execution

--- main_list.py
+++ main_list.py
@@ -1,7 +1,12 @@
 import os
+import argparse
 
-config_list_name = "/cfg/lists/A/"
+parser = argparse.ArgumentParser()
+parser.add_argument("--configlistdir", default="cfg/lists/A/" ,help="Path of the config file dir")
+parser.add_argument("--trainid", default="0" ,help="Trainid")
+args = parser.parse_args()
 
 for i in range(3):
-    #print("python3 main.py --configfile " + config_list_name + str(i+1) + ".yaml")
-    os.system("python3 main.py --configfile " + config_list_name + str(i+1) + ".yaml")
+    #print("python3 main.py --configfile " + args.configlistdir + str(i+1) + ".yaml --trainid " + str(args.trainid))
+    os.system("python3 main.py --configfile " + args.configlistdir + str(i+1) + ".yaml --trainid " + str(args.trainid))
+
