daf25811b2c30a12e8fa0dcd453aa27fc27ec272
Daniel Horvath <hdaniel@sztaki.hu>
2023-09-19 15:20:39 +0200

    save tb data as csv after training

--- rltrain/logger/logger.py
+++ rltrain/logger/logger.py
@@ -3,6 +3,10 @@ import numpy as np
 import yaml
 import time
 import logging
+import pandas as pd
+from collections import defaultdict
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
 from PIL import Image
 from torch.utils.tensorboard import SummaryWriter
 
@@ -64,7 +68,8 @@ class Logger:
             self.create_folder(os.path.join(self.current_dir,self.logdir, self.exp_name,self.trainid,"model_backup"))    
 
             # Tensorboard
-            self.writer = SummaryWriter(log_dir = os.path.join(self.current_dir,self.logdir,self.exp_name,self.trainid,"runs"))     
+            self.tb_logdir = os.path.join(self.current_dir,self.logdir,self.exp_name,self.trainid,"runs")
+            self.writer = SummaryWriter(log_dir = self.tb_logdir)     
         
         # Printout logging
         log_file_path = os.path.join(self.current_dir,self.logdir, self.exp_name,self.trainid,'logs.log')
@@ -246,6 +251,53 @@ class Logger:
             np.save(f, objs_poses_pixel)
     
     
+    def tb_tabulate_events(self,dpath):
+        summary_iterators = [EventAccumulator(os.path.join(dpath, dname)).Reload() for dname in os.listdir(dpath)]
+
+        # print(summary_iterators[0].Tags())
+
+        # assert False
+
+        tags = summary_iterators[0].Tags()['scalars']
+
+        for it in summary_iterators:
+            assert it.Tags()['scalars'] == tags
+
+        out = defaultdict(list)
+        steps = []
+
+        for tag in tags:
+            steps = [e.step for e in summary_iterators[0].Scalars(tag)]
+
+            for events in zip(*[acc.Scalars(tag) for acc in summary_iterators]):
+                assert len(set(e.step for e in events)) == 1
+
+                out[tag].append([e.value for e in events])
+
+        return out, steps
+
+    def tb_to_csv(self,dpath):
+        dirs = os.listdir(dpath)
+
+        d, steps = self.tb_tabulate_events(dpath)
+        tags, values = zip(*d.items())
+        np_values = np.array(values)
+
+        for index, tag in enumerate(tags):
+            df = pd.DataFrame(np_values[index], index=steps, columns=dirs)
+            df.to_csv(self.tb_get_file_path(dpath, tag))
+
+
+    def tb_get_file_path(self,dpath, tag):
+        file_name = tag.replace("/", "_") + '.csv'
+        folder_path = os.path.join(dpath, 'csv')
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+        print(folder_path)
+        return os.path.join(folder_path, file_name)
+    
+    
     def tb_close(self):
         self.writer.flush()
         self.writer.close()
+        self.tb_to_csv(self.tb_logdir)
