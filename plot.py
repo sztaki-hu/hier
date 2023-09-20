c6b6708c994eb711dc0186edd01403acaf1ae540
Daniel Horvath <hdaniel@sztaki.hu>
2023-09-20 14:25:45 +0200

    plot list of results

--- plot.py
+++ plot.py
@@ -21,10 +21,13 @@ def create_folder(path):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--plotid", default="test01" ,help="Id of plot")
-parser.add_argument("--title", default="Success rate" ,help="Title of the plots")
-parser.add_argument("--logdir", default="csv_to_plot" ,help="Path of the data folder")
 parser.add_argument("--outdir", default="plots" ,help="Path of the output folder")
+parser.add_argument("--show", default=False ,help="Id of plot")
 args = parser.parse_args()
+
+plotdata_list = ['eval_success_rate',"rollout_success_rate","cl_ratio"]
+
+
 
 create_folder(os.path.join(current_dir, args.outdir))
 
@@ -37,76 +40,77 @@ taskname = 'Push'
 her = "final"
 exps.append({"exp_name": exp_id+"_Panda"+taskname+"-v3_sac_"+her+"_linear", "seed_num":2, "color": "blue", "plot_name": "linear"})
 exps.append({"exp_name": exp_id+"_Panda"+taskname+"-v3_sac_"+her+"_selfpaced", "seed_num":2, "color": "green", "plot_name": "selfpaced"})
-exps.append({"exp_name": exp_id+"_Panda"+taskname+"-v3_sac_"+her+"_nocl", "seed_num":2, "color": "purple", "plot_name": "nocl"})
+#exps.append({"exp_name": exp_id+"_Panda"+taskname+"-v3_sac_"+her+"_nocl", "seed_num":2, "color": "purple", "plot_name": "nocl"})
 
 exp_test_color_list = []
 for i in range(len(exps)):
     exp_test_color_list.append(exps[i]['color'])
 
+for plotdata in plotdata_list:
 
-dtypes = np.dtype(
-    [
-        ("exp_name", str),
-        ("seed", int),
-        ("t", int),
-        ("value", float),
-        ("plot_name", str),
-    ]
-)
-data_pd = pd.DataFrame(np.empty(0, dtype=dtypes))
-print(data_pd.dtypes)
+    dtypes = np.dtype(
+        [
+            ("exp_name", str),
+            ("seed", int),
+            ("t", int),
+            ("value", float),
+            ("plot_name", str),
+        ]
+    )
+    data_pd = pd.DataFrame(np.empty(0, dtype=dtypes))
+    print(data_pd.dtypes)
 
-for i in range(len(exps)):
-    running_id = 0
-    for j in range(exps[i]['seed_num']):
-        path = os.path.join(current_dir, "logs",exps[i]['exp_name'],str(j),'runs','csv','eval_success_rate.csv')
-        print(path)
-        pivot = pd.read_csv(path)
+    for i in range(len(exps)):
+        running_id = 0
+        for j in range(exps[i]['seed_num']):
+            path = os.path.join(current_dir, "logs",exps[i]['exp_name'],str(j),'runs','csv',plotdata+'.csv')
+            print(path)
+            pivot = pd.read_csv(path)
 
-        column_names = list(pivot.columns)
-        pivot = pivot.rename(columns={column_names[0]: 't', column_names[1]: "value"})
-        pivot['exp_name'] = exps[i]['exp_name']
-        pivot['plot_name'] = exps[i]['plot_name']
-        pivot['seed'] = int(j)
-        pivot = pivot[['exp_name','seed','t','value','plot_name']]
+            column_names = list(pivot.columns)
+            pivot = pivot.rename(columns={column_names[0]: 't', column_names[1]: "value"})
+            pivot['exp_name'] = exps[i]['exp_name']
+            pivot['plot_name'] = exps[i]['plot_name']
+            pivot['seed'] = int(j)
+            pivot = pivot[['exp_name','seed','t','value','plot_name']]
 
-        data_pd = data_pd.append(pivot, ignore_index = True)
+            data_pd = data_pd.append(pivot, ignore_index = True)
 
+                
+    print(data_pd.to_string())
+    print(data_pd.head())
+
+    # Separate plotting ########################## 
+
+    fig, _ = plt.subplots(figsize=(10,8))
+    #exps_names = data_pd['exp_name'].unique()
+
+    for i in range(len(exps)):
+        for j in range(exps[i]['seed_num']):
+            pivot=data_pd[data_pd["exp_name"] == exps[i]['exp_name']]  
+            pivot=pivot[pivot["seed"] == j]
+            plt.plot(pivot['t'],pivot['value'],color=exps[i]['color'],label=exps[i]['plot_name']) if j == 0 else plt.plot(pivot['t'],pivot['value'],color=exps[i]['color'])
             
-print(data_pd.to_string())
-print(data_pd.head())
 
-# Separate plotting ########################## 
+    plt.legend(title='Labels', bbox_to_anchor=(1, 1.01), loc='upper left')
+    plt.xlabel("t")
+    plt.ylabel(plotdata)
+    plt.title(plotdata)
+    figname = args.plotid + "_" + plotdata + '_.png'
+    plt.savefig(os.path.join(current_dir, args.outdir, figname), bbox_inches='tight')
+    if args.show: plt.show()
 
-fig, _ = plt.subplots(figsize=(10,8))
-#exps_names = data_pd['exp_name'].unique()
+    # SD plot ###################################
 
-for i in range(len(exps)):
-    for j in range(exps[i]['seed_num']):
-        pivot=data_pd[data_pd["exp_name"] == exps[i]['exp_name']]  
-        pivot=pivot[pivot["seed"] == j]
-        plt.plot(pivot['t'],pivot['value'],color=exps[i]['color'],label=exps[i]['plot_name']) if j == 0 else plt.plot(pivot['t'],pivot['value'],color=exps[i]['color'])
-        
+    fig, _ = plt.subplots(figsize=(10,8))
 
-plt.legend(title='Labels', bbox_to_anchor=(1, 1.01), loc='upper left')
-plt.xlabel("t")
-plt.ylabel("Success rate")
-plt.title(args.title)
-figname = args.plotid + '_success_rate.png'
-plt.savefig(os.path.join(current_dir, args.outdir, figname), bbox_inches='tight')
-plt.show()
+    sns.lineplot(data=data_pd, x="t", y="value", hue="plot_name", errorbar=('ci', 95), palette=exp_test_color_list)
 
-# SD plot ###################################
-
-fig, _ = plt.subplots(figsize=(10,8))
-
-sns.lineplot(data=data_pd, x="t", y="value", hue="plot_name", errorbar=('ci', 95), palette=exp_test_color_list)
-
-plt.legend(title='Labels', bbox_to_anchor=(1, 1.01), loc='upper left')
-plt.xlabel("t")
-plt.ylabel("Success rate")
-plt.title(args.title + " with sd")
-figname = args.plotid + '_success_rate_std.png'
-plt.savefig(os.path.join(current_dir, args.outdir, figname),bbox_inches='tight')
-plt.show()
+    plt.legend(title='Labels', bbox_to_anchor=(1, 1.01), loc='upper left')
+    plt.xlabel("t")
+    plt.ylabel(plotdata)
+    plt.title(plotdata + " with sd")
+    figname = args.plotid + '_'+plotdata+'_std.png'
+    plt.savefig(os.path.join(current_dir, args.outdir, figname),bbox_inches='tight')
+    if args.show: plt.show()
 
